ff2e16bb6577d8cecc15ead1a74aa6baa1a3f983
justinasvalaika <48714302+justinasvalaika@users.noreply.github.com>
2019-04-03 22:18:03 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,139 @@
+
+//#include <stdio.h>
+//#include <cmath>
+//#include <cstring>
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#define debugas false;
+using namespace std;
+
+struct Sduomenys {
+	string vardas, pavarde;
+	vector <int> pazymiai;
+	double vidurkis, mediana, galutinis;
+	void pildymas();
+};
+
+vector<Sduomenys> lentele;
+
+void Sduomenys::pildymas()
+{
+	int itemp, j=1;
+	cout << "Vardas: ";
+	cin >> vardas;
+	cout << "Pavarde: ";
+	cin >> pavarde;
+	pazymiai.clear();
+	do {
+		cout << j<<" -as ND: ";
+		cin >> itemp;
+		if (itemp > 0 && itemp <= 10)
+			{
+			    pazymiai.push_back(itemp);
+			    j++;
+			}
+	} while (itemp != 0);
+	vidurkis = 0;
+	for (int i : pazymiai)
+		vidurkis += i;
+	vidurkis = double(vidurkis) / pazymiai.size();
+	sort(pazymiai.begin(), pazymiai.end());
+	mediana = (pazymiai.size() % 2 == 1) ? pazymiai[pazymiai.size() / 2] : double(pazymiai[pazymiai.size() / 2] + pazymiai[pazymiai.size() / 2 - 1]) / 2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+void galutinis(int l)
+{
+
+}
+
+void nuskaitymas()
+{
+
+}
+
+void mediana()
+{
+
+}
+
+void isvedimas()
+{
+    char vm='l';
+    bool _med=false;
+
+
+       cout<<"Pasirink viduri/mediana  V/M:"<<endl;
+       cin>>vm;
+
+    cout<<left<<setw(15)<<"Vardas"<<left<<setw(15)<<"Pavarde"<<right<<setw(15)<<"Galutinis ";
+
+
+    switch (vm)
+    {
+        case ('V','v'):
+            cout<<"(vid.)"<<endl;
+            break;
+        case ('M','m'):
+            _med=true;
+            cout<<"(med.)"<<endl;
+            break;
+    }
+
+    cout<<"-"<<setfill('-')<<setw(50)<<"-"<<endl;
+    cout<<setfill(' ');
+    for (Sduomenys i : lentele) {
+		cout <<left<<setw(15)<< i.vardas<<left<<setw(15)<<i.pavarde<<right<<setw(21);
+
+		_med==false? cout << fixed<<setprecision(2)<<i.vidurkis<<endl: cout<<fixed<< setprecision(2)<<i.mediana<< endl;
+	}
+	cout<<"-"<<setfill('-')<<setw(50)<<"-"<<endl;
+	cout<<setfill(' ');
+}
+
+int main()
+{
+	Sduomenys Sd_temp;  char kr;
+	do {
+		Sd_temp.pildymas();
+		lentele.push_back(Sd_temp);
+
+		cout << "Kitas irasas? T/n: "<<endl;;
+		cin >> kr;
+	} while (kr == 't' || kr == 'T');
+
+
+
+
+
+	nuskaitymas();
+	isvedimas();
+	mediana();
+}
+
+
+/*
+
+
+#ifdef debugas
+	std::cout << "rofl";
+#endif
+for (int j : i.pazymiai)
+			cout << j << " | ";
+		cout << endl;
+*/
+
